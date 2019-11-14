ce03820c93756df77b72b4d8bfc4cb817ed8ad39
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-14 14:39:27 -0500

    add links resources to routes, run migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :links
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  resources :links
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "link#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_192132) do
+ActiveRecord::Schema.define(version: 2019_11_14_193519) do
+
+  create_table "links", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
