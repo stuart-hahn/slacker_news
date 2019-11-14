e0b805cb2524b0afd40dfe35d74860202f6d044b
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-14 14:35:31 -0500

    generate link resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :links
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "link#index"
