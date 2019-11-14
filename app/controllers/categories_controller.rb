8c4aa9025dc40c869b4c710d626fd517fa1c6f83
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-14 16:16:06 -0500

    get new category form working

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,28 @@
 class CategoriesController < ApplicationController
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            redirect_to category_path(@category)
+        else
+            render :new, alert: "Failed to create Category"
+        end
+    end
+
+    def show
+        @category = Category.find_by(id: params[:id])
+    end
+
+    def index
+        @categories = Category.all
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:title)
+    end
 end
