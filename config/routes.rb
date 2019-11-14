f31575ce1c334a17dd21e0cb7e219bf554a20965
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-14 15:53:52 -0500

    add default CRUD views for categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+
   resources :categories
   resources :comments
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :links
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root "links#index"
 end
